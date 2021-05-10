d59be52735db7dcb43e6873872bddf3bf03267f2
Utkarsh Sharma <utkarsh.sharma@webshar.org>
2021-05-10 19:48:57 +0530

    alarm error fix

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ MIN_AGE_LIMIT = 18 # 18 or 45
 DISTRICT_ID = '188' # code for gurgaon. #247
 PINCODE = '12001' # pincode of gurgaon.
 SEARCH_BY = 'DISTRICT' # DISTRICT or PINCODE
-DATE = '11-05-2021' # dd-mm-yyyy
+DATE = '10-05-2021' # dd-mm-yyyy
