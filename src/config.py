c2cb353683eca2f970c2aad531974ddf935e310a
Utkarsh Sharma <utkarsh.sharma@webshar.org>
2021-05-11 15:16:47 +0530

    Bug fix

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 MIN_AGE_LIMIT = 18 # 18 or 45
-DISTRICT_ID = '725' # code for gurgaon. #247
+DISTRICT_ID = '188' # code for gurgaon. #247
 PINCODE = '122001' # pincode of gurgaon.
 SEARCH_BY = 'PINCODE' # DISTRICT or PINCODE
 DATE = '11-05-2021' # dd-mm-yyyy
